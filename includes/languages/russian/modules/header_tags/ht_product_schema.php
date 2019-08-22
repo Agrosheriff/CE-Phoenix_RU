a8ac259c6fd55f518ac9ccd19ecc2699f78c1841
Agrosheriff <ffr@agrosheriff.com>
2019-08-23 00:03:31 +0300

    Add files via upload

--- includes/languages/russian/modules/header_tags/ht_product_schema.php
+++ includes/languages/russian/modules/header_tags/ht_product_schema.php
@@ -11,7 +11,7 @@
 */
 
   define('MODULE_HEADER_TAGS_PRODUCT_SCHEMA_TITLE', 'Продукт-схема JSON-LD');
-  define('MODULE_HEADER_TAGS_PRODUCT_SCHEMA_DESCRIPTION', 'Добавить Продукт-схема JSON-LD для страниц product_info и product_reviews, это хорошо для Google Rich Snippets и product cards.');
+  define('MODULE_HEADER_TAGS_PRODUCT_SCHEMA_DESCRIPTION', 'Добавить Продукт-схема JSON-LD для страницы product_info, это хорошая опция для Google Rich Snippets и для сайтов, которые используют Product Cards.');
 
   define('MODULE_HEADER_TAGS_PRODUCT_SCHEMA_TEXT_IN_STOCK', 'https://schema.org/InStock');
   define('MODULE_HEADER_TAGS_PRODUCT_SCHEMA_TEXT_OUT_OF_STOCK', 'https://schema.org/OutOfStock');

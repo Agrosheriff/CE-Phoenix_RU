fa7fef2b38c71685399fbf34a80149463a2e5935
Agrosheriff <ffr@agrosheriff.com>
2019-08-23 00:00:39 +0300

    Add files via upload

---
+++ includes/languages/russian/modules/content/shopping_cart/cm_sc_stock_notice.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  $Id$
+
+  Copyright (c) 2016:
+    Dan Cole - @Dan Cole
+    James Keebaugh - @kymation
+    Lambros - @Tsimi
+    Rainer Schmied - @raiwa
+
+  All rights reserved.
+  
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+  define('MODULE_CONTENT_SC_STOCK_NOTICE_TITLE', 'Нет в наличии');
+  define('MODULE_CONTENT_SC_STOCK_NOTICE_DESCRIPTION', 'Показать уведомление о наличии на странице корзины.');
+  
+  define('MODULE_CONTENT_SC_STOCK_NOTICE_CANT_CHECKOUT', '<p class="lead">Продукты отмечены ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' отсутствует в нужном количестве на нашем складе.</p><p>Пожалуйста, измените количество продуктов, отмеченных ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ', спасибо.</p>');
+  define('MODULE_CONTENT_SC_STOCK_NOTICE_CAN_CHECKOUT', '<p class="lead">Продукты отмечены ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' отсутствует в нужном количестве на нашем складе.</p><p>Вы можете купить их в любом случае и проверить количество у нас на складе для немедленной доставки в процессе оформления заказа.</p>');

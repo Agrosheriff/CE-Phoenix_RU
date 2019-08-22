c7c569fca34040a358ce9761a07818c4f38c220f
Agrosheriff <ffr@agrosheriff.com>
2019-08-22 23:49:10 +0300

    Add files via upload

--- includes/languages/russian/modules/boxes/bm_reviews.php
+++ includes/languages/russian/modules/boxes/bm_reviews.php
@@ -11,8 +11,9 @@
 */
 
   define('MODULE_BOXES_REVIEWS_TITLE', 'Отзывы');
-  define('MODULE_BOXES_REVIEWS_DESCRIPTION', 'Показывать Отзывы на товары');
+  define('MODULE_BOXES_REVIEWS_DESCRIPTION', 'Показать отзывы о продуктах');
+  
   define('MODULE_BOXES_REVIEWS_BOX_TITLE', 'Отзывы');
-  define('MODULE_BOXES_REVIEWS_BOX_WRITE_REVIEW', 'Написать отзыв!');
-  define('MODULE_BOXES_REVIEWS_BOX_NO_REVIEWS', 'Ещё нет ни одного отзыва');
-  define('MODULE_BOXES_REVIEWS_BOX_TEXT_OF_5_STARS', '%s из 5 Звёзд!');
+  define('MODULE_BOXES_REVIEWS_BOX_WRITE_REVIEW', 'Напишите ваш отзыв о этом продукте!');
+  define('MODULE_BOXES_REVIEWS_BOX_NO_REVIEWS', 'Еще нет отзывов о продукте');
+  define('MODULE_BOXES_REVIEWS_BOX_TEXT_OF_5_STARS', '%s 5 звезд!');
--- includes/languages/russian/modules/boxes/bm_specials.php
+++ includes/languages/russian/modules/boxes/bm_specials.php
@@ -5,11 +5,12 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2018 osCommerce
+  Copyright (c) 2019 osCommerce
 
   Released under the GNU General Public License
 */
 
   define('MODULE_BOXES_SPECIALS_TITLE', 'Скидки');
   define('MODULE_BOXES_SPECIALS_DESCRIPTION', 'Показывать товары со скидками');
-  define('MODULE_BOXES_SPECIALS_BOX_TITLE', 'Скидки');
+  
+  define('MODULE_BOXES_SPECIALS_BOX_TITLE', '<a href="%s">Специальные предложения</a>');

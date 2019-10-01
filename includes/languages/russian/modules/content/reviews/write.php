5bae4bbba8b6f558a4d7379ab894259a94ca748b
Agrosheriff <ffr@agrosheriff.com>
2019-10-01 08:28:02 +0300

    Add files via upload

---
+++ includes/languages/russian/modules/content/reviews/write.php
@@ -0,0 +1,31 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2018 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Написать отзыв');
+
+define('SUB_TITLE_FROM', 'От');
+define('SUB_TITLE_REVIEW', 'Ваш отзыв');
+
+define('SUB_TITLE_RATING', 'Рейтинг');
+define('SUB_TITLE_REVIEW_TEXT', 'Ваш отзыв...');
+
+define('TEXT_BAD', '%s (плохо)');
+define('TEXT_GOOD', '%s (Отлично)');
+
+define('IMAGE_BUTTON_ADD_REVIEW', 'Отправить отзыв владельцу магазина');
+
+define('TEXT_REVIEW_RECEIVED', '%s благодарим вас за ваш отзыв. Он был отправлен владельцу магазина для одобрения и вскоре появится на сайте.');
+
+define('TEXT_REVIEW_WRITING', '<p class="lead">%s, пожалуйста, дайте нам знать ваше мнение и рейтинг на %s - спасибо!</p><p>После того, как вы нажмете кнопку «Отправить», ваш отзыв перейдет к владельцу магазина и вскоре появится на сайте.</p>');
+
+define('TEXT_NOT_PURCHASED', '%s похоже, что есть проблема! Вы еще не купили этот продукт!');
+define('TEXT_ALREADY_REVIEWED', '%s похоже, что есть проблема! Вы уже дали отзыв для этого продукта!');

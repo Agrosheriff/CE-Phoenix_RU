5ab74f38f492d875cf5c0f3e9a699722c6243e20
Agrosheriff <ffr@agrosheriff.com>
2019-10-01 08:20:16 +0300

    Add files via upload

--- includes/languages/russian/modules/content/account/cm_account_sage_pay_cards.php
+++ includes/languages/russian/modules/content/account/cm_account_sage_pay_cards.php
@@ -16,11 +16,15 @@
   define('MODULE_CONTENT_ACCOUNT_SAGE_PAY_CARDS_ERROR_MAIN_MODULE', 'Этот модуль не будет загружен до тех пор, пока не будет установлен, сконфигрурирован и разрешён платёжный модуль Sage Pay Direct. Установите и сконфигурируйте модуль оплаты Sage Pay Direct.');
 
   define('MODULE_CONTENT_ACCOUNT_SAGE_PAY_CARDS_LINK_TITLE', 'Управление сохранёнными платёжными картами.');
+
   define('MODULE_CONTENT_ACCOUNT_SAGE_PAY_CARDS_NAVBAR_TITLE_1', 'Мой аккаунт');
   define('MODULE_CONTENT_ACCOUNT_SAGE_PAY_CARDS_NAVBAR_TITLE_2', 'Сохранённые карты');
+
   define('MODULE_CONTENT_ACCOUNT_SAGE_PAY_CARDS_HEADING_TITLE', 'Сохранённые карты');
   define('MODULE_CONTENT_ACCOUNT_SAGE_PAY_CARDS_TEXT_DESCRIPTION', '<p>Сохраненные платежные карты хранятся надежно и безопасно в сертифицированной PCI DSS compliant среде. Высокий уровень безопасности обеспечивает удобное использование Сохраненных карт при последующих покупках без повторного ввода данных карты.</p><p>Новые карты могут быть надежно сохранены для использования в следующих заказах.</p>');
+
   define('MODULE_CONTENT_ACCOUNT_SAGE_PAY_CARDS_SAVED_CARDS_TITLE', 'Сохранённые карты');
   define('MODULE_CONTENT_ACCOUNT_SAGE_PAY_CARDS_TEXT_NO_CARDS', '<p>Нет сохранённых карт.</p>');
+
   define('MODULE_CONTENT_ACCOUNT_SAGE_PAY_CARDS_SUCCESS_DELETED', 'Карта удалена.');
 ?>
--- includes/languages/russian/modules/content/account/cm_account_set_password.php
+++ includes/languages/russian/modules/content/account/cm_account_set_password.php
@@ -12,9 +12,15 @@
 
   define('MODULE_CONTENT_ACCOUNT_SET_PASSWORD_TITLE', 'Установить пароль личного Кабинета');
   define('MODULE_CONTENT_ACCOUNT_SET_PASSWORD_DESCRIPTION', 'Поменять страницу Изменить пароль на Установить пароль если пароль небыл задан ранее.');
+
   define('MODULE_CONTENT_ACCOUNT_SET_PASSWORD_SET_PASSWORD_LINK_TITLE', 'Задать пароль к личному Кабинету.');
+
   define('MODULE_CONTENT_ACCOUNT_SET_PASSWORD_NAVBAR_TITLE_1', 'Мой Кабинет');
   define('MODULE_CONTENT_ACCOUNT_SET_PASSWORD_NAVBAR_TITLE_2', 'Задать пароль');
+
   define('MODULE_CONTENT_ACCOUNT_SET_PASSWORD_HEADING_TITLE', 'Задать пароль');
+
   define('MODULE_CONTENT_ACCOUNT_SET_PASSWORD_SET_PASSWORD_TITLE', 'Задать пароль');
+
   define('MODULE_CONTENT_ACCOUNT_SET_PASSWORD_SUCCESS_PASSWORD_SET', 'Ваш пароль сохранен.');
+?>

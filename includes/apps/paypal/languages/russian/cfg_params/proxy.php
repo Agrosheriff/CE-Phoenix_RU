adacf4b1ed1a8ae0e7a1f76b043ab9eb1181b3e1
Agrosheriff <ffr@agrosheriff.com>
2019-10-01 08:48:39 +0300

    Add files via upload

--- includes/apps/paypal/languages/russian/cfg_params/proxy.php
+++ includes/apps/paypal/languages/russian/cfg_params/proxy.php
@@ -1,2 +1,2 @@
 cfg_proxy_title = Proxy
-cfg_proxy_desc = Отправьте запросы API Curl через этот прокси-сервер. Отправить Curl запросы API через этот прокси-сервер. (host:port, напр.: 123.45.67.89:8080 или proxy.example.com:8080)
+cfg_proxy_desc = Отправить Curl API запросы через этот прокси-сервер. (host:port, напр.: 123.45.67.89:8080 или proxy.example.com:8080)
--- includes/apps/paypal/languages/russian/cfg_params/verify_ssl.php
+++ includes/apps/paypal/languages/russian/cfg_params/verify_ssl.php
@@ -1,5 +1,5 @@
 cfg_verify_ssl_title = Проверка SSL
-cfg_verify_ssl_desc = Установите это на Вкл. для проверки сертификата SSL при выполнении API вызовов к серверам системы PayPal.
+cfg_verify_ssl_desc = Включите это для проверки сертификата SSL при выполнении API вызовов к серверам системы PayPal.
 
 cfg_verify_ssl_true = Вкл.
 cfg_verify_ssl_false = Выкл.

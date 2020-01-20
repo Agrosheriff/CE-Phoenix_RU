94cb188aa37b4585c7d0b52447554fb81cd7b378
Agrosheriff <ffr@agrosheriff.com>
2020-01-21 00:11:11 +0200

    Add files via upload
    1.0.4.3 update

---
+++ includes/languages/russian/hooks/shop/siteWide/jcm_google_recaptcha.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+
+  Released under the GNU General Public License
+  Updated for Phoniex Hooks V1.0 by JcMagie 12-01-2020. Ru translated Fred.
+*/
+
+//google
+define('MODULE_CONTENT_RECAPTCHA_ERROR','Извините, вы должны доказать, что вы человек, а не робот. Заполните пожалуйста Google Captcha');
+define('MODULE_CONTENT_RECAPTCHA_SITEKEY','--- Вставить публичный ключ');
+define('MODULE_CONTENT_RECAPTCHA_SECRETKEY','--- вставить секретный ключ');

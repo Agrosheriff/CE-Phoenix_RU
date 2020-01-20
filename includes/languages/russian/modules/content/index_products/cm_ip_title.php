34d05c19aae1a9c970a7b182f3313bf9e0328455
Agrosheriff <ffr@agrosheriff.com>
2020-01-21 00:27:30 +0200

    Add files via upload
    Update

--- includes/languages/russian/modules/content/index_products/cm_ip_title.php
+++ includes/languages/russian/modules/content/index_products/cm_ip_title.php
@@ -8,8 +8,8 @@ Translation to RU from Fredi. Updated 7/01/2020
 Released under the GNU General Public License
 */
 
-define('MODULE_CONTENT_IP_TITLE_TITLE        = 'Заголовок страницы');
-define('MODULE_CONTENT_IP_TITLE_DESCRIPTION  = 'Показать название категорий/производителей на страницах товаров.');
+define('MODULE_CONTENT_IP_TITLE_TITLE', 'Заголовок страницы');
+define('MODULE_CONTENT_IP_TITLE_DESCRIPTION', 'Показать название категорий/производителей на страницах товаров.');
 
-define('MODULE_CONTENT_IP_TITLE_PUBLIC_TITLE = '%s');
+define('MODULE_CONTENT_IP_TITLE_PUBLIC_TITLE', '%s');
 

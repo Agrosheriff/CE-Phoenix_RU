31d4aeb28d0371c24ea67646c95915f002503c4d
Agrosheriff <ffr@agrosheriff.com>
2020-01-22 23:06:13 +0200

    Add files via upload

--- includes/languages/russian/modules/content/login/cm_forgot_password.php
+++ includes/languages/russian/modules/content/login/cm_forgot_password.php
@@ -11,6 +11,6 @@ Released under the GNU General Public License
 define('MODULE_CONTENT_FORGOT_PASSWORD_TITLE', 'Забыли пароль');
 define('MODULE_CONTENT_FORGOT_PASSWORD_DESCRIPTION', 'Показать ссылку, чтобы позволить клиенту сбросить пароль на странице входа');
 
-define('MODULE_CONTENT_FORGOT_PASSWORD_INTRO_TEXT', 'Забыли ваш пароль?  Нет проблем!');
-define('MODULE_CONTENT_FORGOT_PASSWORD_BUTTON_TEXT', 'Обновить ваш пароль');
+define('MODULE_CONTENT_FORGOT_PASSWORD_INTRO_TEXT', 'Забыли пароль?  Нет проблем!');
+define('MODULE_CONTENT_FORGOT_PASSWORD_BUTTON_TEXT', 'Обновить Ваш пароль');
 

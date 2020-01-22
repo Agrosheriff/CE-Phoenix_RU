2eda491bc1c3ef73c5503eedbdd86ec3fd15d7ed
Agrosheriff <ffr@agrosheriff.com>
2020-01-22 17:03:16 +0200

    Add files via upload

--- includes/languages/russian/modules/content/shopping_cart/cm_sc_stock_notice.php
+++ includes/languages/russian/modules/content/shopping_cart/cm_sc_stock_notice.php
@@ -14,5 +14,5 @@
   define('MODULE_CONTENT_SC_STOCK_NOTICE_TITLE', 'Товара нет в наличии');
   define('MODULE_CONTENT_SC_STOCK_NOTICE_DESCRIPTION', 'Показать уведомление о наличии Товара на странице корзины.');
   
-  define('MODULE_CONTENT_SC_STOCK_NOTICE_CANT_CHECKOUT', '<p class="lead">Товары отмечены ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' отсутствует в нужном количестве на нашем складе.</p><p>Пожалуйста, измените количество продуктов, отмеченных ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ', спасибо.</p>');
+  define('MODULE_CONTENT_SC_STOCK_NOTICE_CANT_CHECKOUT', '<p class="lead">Товары отмечены ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' отсутствует в нужном количестве на нашем складе.</p><p>Пожалуйста, измените количество товаров, отмеченных ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ', спасибо.</p>');
   define('MODULE_CONTENT_SC_STOCK_NOTICE_CAN_CHECKOUT', '<p class="lead">Товары отмечены ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' отсутствует в нужном количестве на нашем складе.</p><p>Вы можете купить их в любом случае и проверить количество у нас на складе для немедленной доставки в процессе оформления заказа.</p>');

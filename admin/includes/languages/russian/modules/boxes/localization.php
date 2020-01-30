efd2be63033ce88adf5e1bf90b8f7cef64e076a5
Agrosheriff <ffr@agrosheriff.com>
2020-01-30 20:27:14 +0200

    Add files via upload

--- admin/includes/languages/russian/modules/boxes/localization.php
+++ admin/includes/languages/russian/modules/boxes/localization.php
@@ -10,5 +10,5 @@
   Released under the GNU General Public License
 */
 
-define('BOX_HEADING_LOCALIZATION', '<i class="fas fa-language fa-fw mr-1"></i> Местонахождение');
+define('BOX_HEADING_LOCALIZATION', '<i class="fas fa-language fa-fw mr-1"></i> Локализация');
 

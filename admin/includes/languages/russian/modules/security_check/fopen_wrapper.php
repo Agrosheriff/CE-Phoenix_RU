7fa843c362e265b4e398f65480ed0cb963f4d287
Agrosheriff <ffr@agrosheriff.com>
2020-01-31 13:19:05 +0200

    Add files via upload

---
+++ admin/includes/languages/russian/modules/security_check/fopen_wrapper.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+  Translation to RU from Fredi. Updated 30/01/2020
+  Released under the GNU General Public License
+
+*/
+
+define('MODULE_SECURITY_CHECK_FOPEN_WRAPPER_TITLE', 'allow_url_fopen');
+define('MODULE_SECURITY_CHECK_FOPEN_WRAPPER_ERROR', 'allow_url_fopen должен быть включен в php.ini <br> Это настройка хостинга, которую можно выполнить через панель управления хостингом - если нет, спросите своего хостера.');
+

7566a1b7f0b36a86b4ac95a9cc90bd54c498ca66
Agrosheriff <ffr@agrosheriff.com>
2020-02-08 16:16:34 +0200

    Add files via upload

--- includes/languages/russian/modules/content/product_info/cm_pi_review_stars.php
+++ includes/languages/russian/modules/content/product_info/cm_pi_review_stars.php
@@ -10,12 +10,12 @@
   Released under the GNU General Public License
 */
 
-define('MODULE_CONTENT_PI_REVIEW_STARS_TITLE', 'Ссылка / Обзор Звезд');
+define('MODULE_CONTENT_PI_REVIEW_STARS_TITLE', 'Отображение Звезд и ссылки');
 define('MODULE_CONTENT_PI_REVIEW_STARS_DESCRIPTION', 'Показать звезды, количество отзывов и ссылку для написания отзыва на странице product_info');
 
 define('MODULE_CONTENT_PI_REVIEW_STARS_COUNT', '%s Отзывы');
 define('MODULE_CONTENT_PI_REVIEW_STARS_COUNT_ONE', '%s Отзывы');
 
-define('MODULE_CONTENT_PI_REVIEW_STARS_DO_REVIEW', 'Добвавить Ваш Отзыв');
+define('MODULE_CONTENT_PI_REVIEW_STARS_DO_REVIEW', 'Добавить Ваш Отзыв');
 
 define('MODULE_CONTENT_PI_REVIEW_STARS_DO_FIRST_REVIEW', 'Будьте первым кто оценит этот продукт');

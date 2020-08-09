4450d17d9dd485c48059a7474c982b44531a9675
Agrosheriff <ffr@agrosheriff.com>
2020-08-09 19:22:58 +0300

    Add files via upload

--- includes/languages/russian/modules/content/footer/cm_footer_account.php
+++ includes/languages/russian/modules/content/footer/cm_footer_account.php
@@ -1,22 +1,24 @@
 <?php
 /*
 $Id$
+
 osCommerce, Open Source E-Commerce Solutions
 http://www.oscommerce.com
+
 Copyright (c) 2020 osCommerce
-Translation to RU from Fredi. Updated 2/05/2020
+Translation to RU from Fredi. Updated 7/08/2020
 Released under the GNU General Public License
 */
 
-  define('MODULE_CONTENT_FOOTER_ACCOUNT_TITLE', 'Мой кабинет');
-  define('MODULE_CONTENT_FOOTER_ACCOUNT_DESCRIPTION', 'Добавить Мой кабинет в футер');
+define('MODULE_CONTENT_FOOTER_ACCOUNT_TITLE', 'Мой кабинет');
+define('MODULE_CONTENT_FOOTER_ACCOUNT_DESCRIPTION', 'Добавить Мой кабинет в футер');
 
-  define('MODULE_CONTENT_FOOTER_ACCOUNT_HEADING_TITLE', 'Обслуживание клиентов');
+define('MODULE_CONTENT_FOOTER_ACCOUNT_HEADING_TITLE', 'Обслуживание клиентов');
 
-  define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_ACCOUNT', 'Мой кабинет');
-  define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_ADDRESS_BOOK', 'Моя адресная книга');
-  define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_ORDER_HISTORY', 'Моя история заказов');
-  define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_LOGOFF', 'Безопасный выход');
-  define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_CREATE_ACCOUNT', 'Создать кабинет');
-  define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_LOGIN', 'Вы зарегистрированы? Заходите пожалуйста!');
+define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_ACCOUNT', 'Мой кабинет');
+define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_ADDRESS_BOOK', 'Моя адресная книга');
+define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_ORDER_HISTORY', 'Моя история заказов');
+define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_LOGOFF', 'Безопасный выход');
+define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_CREATE_ACCOUNT', 'Создать кабинет');
+define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_LOGIN', 'Вы зарегистрированы? Заходите пожалуйста!');
 
--- includes/languages/russian/modules/content/footer/cm_footer_information_links.php
+++ includes/languages/russian/modules/content/footer/cm_footer_information_links.php
@@ -1,10 +1,12 @@
 <?php
 /*
 $Id$
+
 osCommerce, Open Source E-Commerce Solutions
 http://www.oscommerce.com
+
 Copyright (c) 2020 osCommerce
-Translation to RU from Fredi. Updated 7/01/2020
+Translation to RU from Fredi. Updated 7/08/2020
 Released under the GNU General Public License
 */
 
@@ -12,10 +14,10 @@ define('MODULE_CONTENT_FOOTER_INFORMATION_TITLE', 'Информация');
 define('MODULE_CONTENT_FOOTER_INFORMATION_DESCRIPTION', 'Добавить блок информационных ссылок в футер сайта');
 
 define('MODULE_CONTENT_FOOTER_INFORMATION_HEADING_TITLE', 'Информация');
-  define('MODULE_CONTENT_FOOTER_INFORMATION_FAQ', 'FAQ Вопросы и ответы');
+define('MODULE_CONTENT_FOOTER_INFORMATION_FAQ', 'FAQ Вопросы и ответы');
 
 const MODULE_CONTENT_FOOTER_INFORMATION_DATA = array(
-  'privacy.php' => 'Безопасность и использование Cookie',
-  'conditions.php' => 'Условия использования',
-  'shipping.php' => 'Доставка и возврат',
-  'contact_us.php' => 'Связаться с нами');
+'privacy.php' => 'Безопасность и использование Cookie',
+'conditions.php' => 'Условия использования',
+'shipping.php' => 'Доставка и возврат',
+'contact_us.php' => 'Связаться с нами');

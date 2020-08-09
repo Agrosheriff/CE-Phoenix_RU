e59b81814a97612138a24ed1c9d3071086dcc324
Agrosheriff <ffr@agrosheriff.com>
2020-08-09 19:31:48 +0300

    Add files via upload

--- includes/languages/russian/modules/content/login/cm_login_form.php
+++ includes/languages/russian/modules/content/login/cm_login_form.php
@@ -5,16 +5,16 @@ $Id$
 osCommerce, Open Source E-Commerce Solutions
 http://www.oscommerce.com
 
-Copyright (c) 2014 osCommerce
-Translation to RU from Fredi. Updated 7/01/2020
+Copyright (c) 2020 osCommerce
+Translation to RU from Fredi. Updated 7/08/2020
 Released under the GNU General Public License
 */
 
 define('MODULE_CONTENT_LOGIN_FORM_TITLE', 'Форма Входа');
 define('MODULE_CONTENT_LOGIN_FORM_DESCRIPTION', 'Показывать форму входа на странице входа');
 define('MODULE_CONTENT_LOGIN_HEADING_RETURNING_CUSTOMER', 'Зарегистрированный покупатель');
-define('MODULE_CONTENT_LOGIN_TEXT_RETURNING_CUSTOMER', 'Я уже покупал в магазине ' . STORE_NAME . '.<br>');
-define('MODULE_CONTENT_LOGIN_TEXT_LOGIN_ERROR', 'Ошибочка вышла: пароль или E-Mail адрес не подходят. Попробуйте поправить или указать другой.');
+define('MODULE_CONTENT_LOGIN_TEXT_RETURNING_CUSTOMER', 'Я уже покупал в магазине.');
+define('MODULE_CONTENT_LOGIN_TEXT_LOGIN_ERROR', 'Ошибочка вышла: пароль или E-Mail адрес не подходят. Попробуйте исправить или указать правильные.');
 
 define('MODULE_CONTENT_LOGIN_ENTRY_EMAIL_ADDRESS_PLACEHOLDER', 'Email');
 define('MODULE_CONTENT_LOGIN_ENTRY_PASSWORD_PLACEHOLDER', 'Пароль');

9ca7cba8fa056df997c8139b085af5aa82db8d01
Agrosheriff <ffr@agrosheriff.com>
2020-08-09 19:32:28 +0300

    Add files via upload

---
+++ includes/languages/russian/modules/content/info/cm_info_text.php
@@ -0,0 +1,14 @@
+<?php
+/*
+$Id$
+
+osCommerce, Open Source E-Commerce Solutions
+http://www.oscommerce.com
+
+Copyright (c) 2020 osCommerce
+Translation to RU from Fredi. Updated 7/08/2020
+Released under the GNU General Public License
+*/
+
+const MODULE_CONTENT_INFO_TEXT_TITLE = 'Текст';
+const MODULE_CONTENT_INFO_TEXT_DESCRIPTION = 'Показать этот текст на странице';
---
+++ includes/languages/russian/modules/content/info/cm_info_title.php
@@ -0,0 +1,14 @@
+<?php
+/*
+$Id$
+
+osCommerce, Open Source E-Commerce Solutions
+http://www.oscommerce.com
+
+Copyright (c) 2020 osCommerce
+Translation to RU from Fredi. Updated 7/08/2020
+Released under the GNU General Public License
+*/
+
+const MODULE_CONTENT_INFO_TITLE_TITLE = 'Заголовок';
+const MODULE_CONTENT_INFO_TITLE_DESCRIPTION = 'Показать заголовок на этой странице';

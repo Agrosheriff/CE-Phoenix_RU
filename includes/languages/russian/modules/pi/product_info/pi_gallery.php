78b6e1a9c910f0ea05ea351fd149f4de163c55a0
Agrosheriff <ffr@agrosheriff.com>
2020-08-09 19:57:47 +0300

    Add files via upload

--- includes/languages/russian/modules/pi/product_info/pi_gallery.php
+++ includes/languages/russian/modules/pi/product_info/pi_gallery.php
@@ -5,12 +5,12 @@ $Id$
 osCommerce, Open Source E-Commerce Solutions
 http://www.oscommerce.com
 
-Copyright (c) 2020 osCommerce
-Translation to RU from Fredi. Updated 2/05/2020
+Copyright (c) 2018 osCommerce
+Translation to RU from Fredi. Updated 7/08/2020
 Released under the GNU General Public License
 */
 
-const PI_GALLERY_TITLE       = 'Галерея';
+const PI_GALLERY_TITLE = 'Галерея';
 const PI_GALLERY_DESCRIPTION = 'Показывает изображения продуктов на странице product_info.<div class="alert alert-info">Дочерний модуль для &pi; system.</div>';
 
 const PI_GALLERY_ALBUM_NAME = 'Альбом для %s';
--- includes/languages/russian/modules/pi/product_info/pi_options_attributes.php
+++ includes/languages/russian/modules/pi/product_info/pi_options_attributes.php
@@ -5,13 +5,13 @@ $Id$
 osCommerce, Open Source E-Commerce Solutions
 http://www.oscommerce.com
 
-Copyright (c) 2020 osCommerce
-Translation to RU from Fredi. Updated 2/05/2020
+Copyright (c) 2018 osCommerce
+Translation to RU from Fredi. Updated 7/08/2020
 Released under the GNU General Public License
 */
 
-const PI_OA_TITLE         = 'Опции и атрибуты';
-const PI_OA_DESCRIPTION   = 'Показывает параметры продукта на странице product_info.<div class="alert alert-info">Это дочерний модуль для использования с &Pi; system.</div>';
+const PI_OA_TITLE = 'Опции и атрибуты';
+const PI_OA_DESCRIPTION = 'Показывает параметры продукта на странице product_info.<div class="alert alert-info">Это дочерний модуль для использования с &Pi; system.</div>';
 
 const PI_OA_HEADING_TITLE = 'Доступные опции';
 

6c2ec3f336d4d590b7ac39b68cdb6284945b562b
Agrosheriff <ffr@agrosheriff.com>
2020-08-09 20:31:33 +0300

    Add files via upload

--- admin/includes/languages/russian/modules/security_check/fopen_wrapper.php
+++ admin/includes/languages/russian/modules/security_check/fopen_wrapper.php
@@ -6,10 +6,10 @@ osCommerce, Open Source E-Commerce Solutions
 http://www.oscommerce.com
 
 Copyright (c) 2020 osCommerce
-Translation to RU from Fredi. Updated 3/07/2020
+Translation to RU from Fredi. Updated 7/08/2020
 Released under the GNU General Public License
 */
 
 define('MODULE_SECURITY_CHECK_FOPEN_WRAPPER_TITLE', 'allow_url_fopen');
-define('MODULE_SECURITY_CHECK_FOPEN_WRAPPER_ERROR', 'allow_url_fopen должен быть включен в php.ini <br> Это настройка хостинга, которую можно выполнить через панель управления хостингом - если нет, спросите своего хостера.');
+define('MODULE_SECURITY_CHECK_FOPEN_WRAPPER_ERROR', 'allow_url_fopen должен быть включен в php.ini <br> Это настройка хостинга, которую можно выполнить через панель управления хостингом - если нет, спросите у службы поддержки своего хостера.');
 
--- admin/includes/languages/russian/modules/security_check/install_directory.php
+++ admin/includes/languages/russian/modules/security_check/install_directory.php
@@ -6,9 +6,9 @@ osCommerce, Open Source E-Commerce Solutions
 http://www.oscommerce.com
 
 Copyright (c) 2020 osCommerce
-Translation to RU from Fredi. Updated 3/07/2020
+Translation to RU from Fredi. Updated 7/08/2020
 Released under the GNU General Public License
 */
 
-define('WARNING_INSTALL_DIRECTORY_EXISTS', 'Папка инсталляции этого магазина: ' . DIR_FS_CATALOG . 'install. Удалите ее для безопасности Вашего сайта.');
+define('WARNING_INSTALL_DIRECTORY_EXISTS', 'Папка инсталляции этого магазина: ' . DIR_FS_CATALOG . 'install. Удалите ее для безопасности с Вашего сайта.');
 ?>

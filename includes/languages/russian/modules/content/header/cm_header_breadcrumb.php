425c55d3020515909236015dd42cae554f9682b7
Agrosheriff <ffr@agrosheriff.com>
2020-08-09 19:25:57 +0300

    Add files via upload

--- includes/languages/russian/modules/content/header/cm_header_breadcrumb.php
+++ includes/languages/russian/modules/content/header/cm_header_breadcrumb.php
@@ -1,16 +1,18 @@
 <?php
 /*
 $Id$
+
 osCommerce, Open Source E-Commerce Solutions
 http://www.oscommerce.com
+
 Copyright (c) 2020 osCommerce
-Translation to RU from Fredi. Updated 3/07/2020
+Translation to RU from Fredi. Updated 7/08/2020
 Released under the GNU General Public License
 */
 
 define('MODULE_CONTENT_HEADER_BREADCRUMB_TITLE', 'Breadcrumb');
 define('MODULE_CONTENT_HEADER_BREADCRUMB_DESCRIPTION', 'Добавить Breadrcumb в шапку сайта.');
 const MODULE_CONTENT_HEADER_BREADCRUMB_TITLES = [
-  '<i class="fas fa-home"></i><span class="sr-only"> Начало</span>' => null,
-  'Каталог' => 'index.php',
+'<i class="fas fa-home"></i><span class="sr-only"> Начало</span>' => null,
+'Каталог' => 'index.php',
 ];

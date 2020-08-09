f49ce52a9b7740cc0a78bad26758bdda6908bcc5
Agrosheriff <ffr@agrosheriff.com>
2020-08-09 19:15:48 +0300

    Add files via upload

--- includes/languages/russian/modules/action_recorder/ar_admin_login.php
+++ includes/languages/russian/modules/action_recorder/ar_admin_login.php
@@ -6,10 +6,10 @@
   http://www.oscommerce.com
 
   Copyright (c) 2010 osCommerce
-  Translation to RU from Fredi. Updated 7/01/2020
+  Translation to RU from Fredi. Updated 7/08/2020
   Released under the GNU General Public License
 */
 
-  define('MODULE_ACTION_RECORDER_ADMIN_LOGIN_TITLE', 'Вход Администратора');
-  define('MODULE_ACTION_RECORDER_ADMIN_LOGIN_DESCRIPTION', 'Администрирование');
+  define('MODULE_ACTION_RECORDER_ADMIN_LOGIN_TITLE', 'Входы в Админ');
+  define('MODULE_ACTION_RECORDER_ADMIN_LOGIN_DESCRIPTION', 'Запись входов в Админ');
 ?>
--- includes/languages/russian/modules/action_recorder/ar_contact_us.php
+++ includes/languages/russian/modules/action_recorder/ar_contact_us.php
@@ -6,10 +6,10 @@
   http://www.oscommerce.com
 
   Copyright (c) 2010 osCommerce
-  Translation to RU from Fredi. Updated 7/01/2020
+  Translation to RU from Fredi. Updated 7/08/2020
   Released under the GNU General Public License
 */
 
   define('MODULE_ACTION_RECORDER_CONTACT_US_TITLE', 'Контакты');
-  define('MODULE_ACTION_RECORDER_CONTACT_US_DESCRIPTION', 'Запись о  выполнении действия - Contact Us feature.');
+  define('MODULE_ACTION_RECORDER_CONTACT_US_DESCRIPTION', 'Запись о  выполнении действия - Contact Us.');
 ?>

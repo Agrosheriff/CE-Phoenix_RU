7439ce5c32858232a3debdb6daddbdb3054d89ab
Agrosheriff <ffr@agrosheriff.com>
2020-08-09 20:01:48 +0300

    Add files via upload

--- includes/languages/russian/hooks/shop/siteWide/MATC.php
+++ includes/languages/russian/hooks/shop/siteWide/MATC.php
@@ -6,7 +6,7 @@ osCommerce, Open Source E-Commerce Solutions
 http://www.oscommerce.com
 
 Copyright (c) 2020 osCommerce
-Translation to RU from Fredi. Updated 07/03/2020
+Translation to RU from Fredi. Updated 07/08/2020
 
 Released under the GNU General Public License
 */
@@ -15,11 +15,5 @@ Released under the GNU General Public License
 const ENTRY_MATC = 'Юридические соглашения';
 const ENTRY_MATC_TEXT = 'Нажмите здесь, что бы подтвердить, что Вы соглашаетесь с нашими <a href="#" role="button" class="card-link ml-0 border-bottom border-primary" data-toggle="modal" data-target="#TCModal">Условиями использования</a> и <a href="#" role="button" class="card-link ml-0 border-bottom border-primary" data-toggle="modal" data-target="#PModal">Политикой конфиденциальности</a>.'; 
 
-// for the modal popups
+// for the modal popup
 const MATC_BUTTON_CLOSE = 'Закрыть';
-
-const MATC_PRIVACY_MODAL_TITLE = 'Конфиденциальность';
-const MATC_PRIVACY_MODAL_TEXT = '<p>Введите сюда текст о Конфиденциальности на вашем сайте...</p>';
-
-const MATC_TERMS_MODAL_TITLE = 'Условия использования';
-const MATC_TERMS_MODAL_TEXT = '<p>Введите сюда текст о Условиях использования на вашем сайте...</p>';

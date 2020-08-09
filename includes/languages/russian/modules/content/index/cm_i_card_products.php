232cd550928a01feecd79dac981e65c8bc85ebb1
Agrosheriff <ffr@agrosheriff.com>
2020-08-09 19:27:36 +0300

    Add files via upload

--- includes/languages/russian/modules/content/index/cm_i_card_products.php
+++ includes/languages/russian/modules/content/index/cm_i_card_products.php
@@ -1,14 +1,16 @@
 <?php
 /*
 $Id$
+
 osCommerce, Open Source E-Commerce Solutions
 http://www.oscommerce.com
+
 Copyright (c) 2020 osCommerce
-Translation to RU from Fredi. Updated 3/07/2020
+Translation to RU from Fredi. Updated 7/08/2020
 Released under the GNU General Public License
 */
 
 define('MODULE_CONTENT_CARD_PRODUCTS_TITLE', 'Новые товары');
-define('MODULE_CONTENT_CARD_PRODUCTS_DESCRIPTION', 'Показать "Новые товары" на Index page.');
+define('MODULE_CONTENT_CARD_PRODUCTS_DESCRIPTION', 'Показать "Новые товары" на главной странице.');
 
 define('MODULE_CONTENT_CARD_PRODUCTS_HEADING', 'Новинки: %s');

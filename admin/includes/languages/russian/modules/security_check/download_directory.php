baa6ac0f58f5e76ef706975e1275a1119f5a736f
Agrosheriff <ffr@agrosheriff.com>
2020-08-26 10:19:11 +0300

    Add files via upload
    Updated

--- admin/includes/languages/russian/modules/security_check/download_directory.php
+++ admin/includes/languages/russian/modules/security_check/download_directory.php
@@ -6,9 +6,10 @@ osCommerce, Open Source E-Commerce Solutions
 http://www.oscommerce.com
 
 Copyright (c) 2020 osCommerce
-Translation to RU from Fredi. Updated 3/07/2020
+Translation to RU from Fredi. Updated 26/08/2020 Thanks wildball. 
 Released under the GNU General Public License
 */
 
-define('WARNING_DOWNLOAD_DIRECTORY_NON_EXISTENT', 'Каталога для виртуальных (загружаемых) товаров не существует: ' . DIR_FS_DOWNLOAD . '. Загружаемые товары не будут работать пока эта директория не создана.');
+const WARNING_DOWNLOAD_DIRECTORY_NON_EXISTENT = 'Каталога для виртуальных (загружаемых) товаров не существует:  [%s]. Загружаемые товары не будут работать пока эта директория не создана.';
+
 ?>

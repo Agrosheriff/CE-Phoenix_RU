d6fca35d7d095f038b03d8bac11d1a5b7834970b
Agrosheriff <fermer@farmgarden.biz>
2021-02-22 21:04:58 +0200

    Add files via upload

--- admin/includes/languages/russian/modules/security_check/session_storage.php
+++ admin/includes/languages/russian/modules/security_check/session_storage.php
@@ -10,6 +10,6 @@ Translation to RU from Fredi. Updated 3/07/2020
 Released under the GNU General Public License
 */
 
-define('WARNING_SESSION_DIRECTORY_NON_EXISTENT', 'Директория сессий отсутствует: ' . tep_session_save_path() . '. Сессии не будут работать пока эта директория не будет создана.');
-define('WARNING_SESSION_DIRECTORY_NOT_WRITEABLE', 'Невозможно записывать файлы в каталог сессий: ' . tep_session_save_path() . ' . Сессии не будут работать, пока необходимые права доступа не будут установлены.');
+define('WARNING_SESSION_DIRECTORY_NON_EXISTENT', 'Директория сессий отсутствует: "%s". Сессии не будут работать пока эта директория не будет создана.');
+define('WARNING_SESSION_DIRECTORY_NOT_WRITEABLE', 'Невозможно записывать файлы в каталог сессий: "%s" . Сессии не будут работать, пока необходимые права доступа не будут установлены.');
 ?>

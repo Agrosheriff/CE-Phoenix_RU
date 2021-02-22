0877b4cb313137f2c2ff2eed476398afb6bf6764
Agrosheriff <fermer@farmgarden.biz>
2021-02-22 21:05:31 +0200

    Add files via upload

--- admin/includes/languages/russian/modules/security_check/download_directory.php
+++ admin/includes/languages/russian/modules/security_check/download_directory.php
@@ -10,5 +10,4 @@ Translation to RU from Fredi. Updated 3/07/2020
 Released under the GNU General Public License
 */
 
-define('WARNING_DOWNLOAD_DIRECTORY_NON_EXISTENT', 'Каталога для виртуальных (загружаемых) товаров не существует: ' . DIR_FS_DOWNLOAD . '. Загружаемые товары не будут работать пока эта директория не создана.');
-?>
+const WARNING_DOWNLOAD_DIRECTORY_NON_EXISTENT = 'Каталога для виртуальных (загружаемых) товаров не существует:  [%s]. Загружаемые товары не будут работать пока эта директория не создана.';

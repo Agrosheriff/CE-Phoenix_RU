f2a9c53215dcf55f5826db021434766a0cdefe60
Agrosheriff <fermer@farmgarden.biz>
2021-03-20 19:06:49 +0200

    Add files via upload
    Solve redirect

--- admin/includes/languages/russian/modules/security_check/extended/shopside_checklist.php
+++ admin/includes/languages/russian/modules/security_check/extended/shopside_checklist.php
@@ -11,5 +11,5 @@ Released under the GNU General Public License
 */
 
 define('MODULE_SECURITY_CHECK_EXTENDED_SHOPSIDE_CHECKLIST_TITLE', 'Контрольный список пользователей');
-define('MODULE_SECURITY_CHECK_EXTENDED_SHOPSIDE_CHECKLIST_MESSAGE', 'Смотрите наш онлайн <a target="_blank" rel="noopener" href="https://github.com/gburton/Responsive-osCommerce#user-checklist"><u>Контрольный список пользователей</u></a> для основного списка задач, которые вы должны выполнить.');
+define('MODULE_SECURITY_CHECK_EXTENDED_SHOPSIDE_CHECKLIST_MESSAGE', 'Смотрите наш онлайн <a target="_blank" rel="noreferrer" href="https://github.com/CE-PhoenixCart/PhoenixCart#user-checklist"><u>Контрольный список пользователей</u></a> для основного списка задач, которые вы должны выполнить.');
 

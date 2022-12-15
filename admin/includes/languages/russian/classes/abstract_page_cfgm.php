d6aefa8d550dec7af0a573c1b14da08bba4f5b68
Agrosheriff <fermer@farmgarden.biz>
2022-12-15 18:39:23 +0200

    Add files via upload

---
+++ admin/includes/languages/russian/classes/abstract_page_cfgm.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2022 Phoenix Cart
+Translation to RU from Fredi. Updated 3/12/22
+
+  Released under the GNU General Public License
+*/
+
+const TEXT_ALL_MODULES = '-- Все модули --';
---
+++ admin/includes/languages/russian/classes/config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $Id$
+
+  CE Phoenix, E-Commerce made Easy
+  https://phoenixcart.org
+
+  Copyright (c) 2022 Phoenix Cart
+Translation to RU from Fredi. Updated 3/12/22
+
+  Released under the GNU General Public License
+*/
+
+const CLASS_CONFIG_TEXT_INVALID_ZONE = 'Неправильная зона для выбранной страны';
